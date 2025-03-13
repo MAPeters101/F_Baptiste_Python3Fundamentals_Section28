8966c337036f9484bc4a85ca694aa411ae6a741f
Mark Peters <mapeters101@earthlink.net>
2025-03-13 17:50:50 -0400

    Descriptions for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,24 @@
+'''
+Question 3
+Use a GET request to this URL:
+
+url = 'https://httpbin.org/json'
+Use the response from that request to:
+
+determine the response format
+extract the response into a Python object
+Question 4
+Use a POST request to call this url:
+
+url = 'https://httpbin.org/anything'
+Make this call passing the following query parameters: a=1 and b=2
+
+Also, pass this dictionary as the body of the post request:
+
+data = {
+    'x': 100,
+    'y': 200,
+    'z': ['a', 'b', 'c']
+}
+Load the returned JSON into a Python object and print it out.
+'''
