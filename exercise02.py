4b4268841b9b2899eb02df332ded1f4283a35ea7
Mark Peters <mapeters101@earthlink.net>
2025-03-13 17:49:57 -0400

    Descriptions for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,14 @@
+'''
+Question 2
+Use the requests library to load the following html page:
+
+url = 'https://en.wikipedia.org/wiki/John_von_Neumann'
+Once you have loaded that page, extract the title of that page, which is the
+text located between the <title> and </title> tags (often referred to as
+opening and closing tags, or start and end tags, respectively).
+
+Hint: You'll want to read the Python docs for the find method available for
+strings:
+
+https://docs.python.org/3/library/stdtypes.html?highlight=string#str.find
+'''
